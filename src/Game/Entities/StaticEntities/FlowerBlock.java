63576a44ee41dc93eb31f6947ef1e2871d31bc06
andrew.gonzalez4 <andrew.gonzalez4@10.31.14.140>
2019-04-09 17:53:36 -0400

    Animated flowerblocks!!! :)

--- src/Game/Entities/StaticEntities/FlowerBlock.java
+++ src/Game/Entities/StaticEntities/FlowerBlock.java
@@ -1,5 +1,7 @@
 package Game.Entities.StaticEntities;
 
+import java.awt.Dimension;
+
 import Game.Entities.DynamicEntities.BaseDynamicEntity;
 import Main.Handler;
 import Resources.Animation;
@@ -9,8 +11,16 @@ public class FlowerBlock extends BaseDynamicEntity {
 
 	public Animation anim;
 	public FlowerBlock(int x, int y, int width, int height, Handler handler) {
-		super(x, y, width, height,handler, Images.flowerBlock[2]);
-		anim = new Animation(160,Images.flowerBlock);
+		super(x, y, width, height,handler, Images.flowerBlock[0]);
+		anim = new Animation(100,Images.flowerBlock);
+	}
+
+	@Override
+	public void tick(){
+
+		super.tick();
+		anim.tick();
+
 	}
 
 }
